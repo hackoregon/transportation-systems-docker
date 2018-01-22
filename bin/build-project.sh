779c50935d2ad4310f0e40ed19808704933abfdc
Brian Grant <brian.grant@urbanairship.com>
2018-01-21 18:33:54 -0800

    move locations and add create command

--- bin/build-project.sh
+++ bin/build-project.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
-usage() { echo "Usage: $0 [-d] for a docs build or [-g] for a geodjango build " 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-d] for a docs build, [-g] for a geodjango build, [-c] to create a django project using the PROJECT_NAME variable in env.sh" 1>&2; exit 1; }
 
 source ./bin/env.sh
 
-while getopts ":dg" opt; do
+while getopts ":dgc" opt; do
     case "$opt" in
         d)
-          docker-compose -f ./composefiles/mkdocs-compose.yml build
+          docker-compose -f ./composefiles/mkdocs-compose.yml -p $PROJECT_NAME build
           ;;
         g)
-          docker-compose -f ./composefiles/geodjango-compose.yml build
+          docker-compose -f ./composefiles/geodjango-compose.yml -p $PROJECT_NAME build
+          ;;
+        c)
+          docker-compose -f ./composefiles/geodjango-compose.yml -p $PROJECT_NAME run api django-admin.py startproject $PROJECT_NAME .
           ;;
         *)
           usage
--- bin/start-project.sh
+++ bin/start-project.sh
@@ -6,13 +6,13 @@ source ./bin/env.sh
 while getopts ":mgtn" opt; do
     case "$opt" in
         m)
-          docker-compose -f ./composefiles/mkdocs-compose.yml up
+          docker-compose -f ./composefiles/mkdocs-compose.yml -p $PROJECT_NAME up
           ;;
         g)
-          docker-compose -f ./composefiles/geodjango-compose.yml up
+          docker-compose -f ./composefiles/geodjango-compose.yml -p $PROJECT_NAME up
           ;;
         t)
-          docker-compose -f ./composefiles/travis-docker-compose.yml up
+          docker-compose -f ./composefiles/travis-docker-compose.yml -p $PROJECT_NAME up
           ;;
         *)
           usage
